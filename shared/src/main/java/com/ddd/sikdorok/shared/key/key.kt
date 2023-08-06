33973f7ef04ba4ced56863565990560292c7b243
TaeseongYun <yts0646@gmail.com>
2023-08-06 22:48:19 +0900

    [ chore ] : 식도록 프리퍼런스 모듈 생성 완료 (#15)
    * [ chore ] : 식도록 프리퍼런스 모듈 생성 완료
    
    * [ Feature ] : 식도록 프리퍼런스 구현 완료

---
+++ shared/src/main/java/com/ddd/sikdorok/shared/key/key.kt
@@ -0,0 +1,7 @@
+package com.ddd.sikdorok.shared.key
+
+object Keys {
+    const val ACCESS_TOKEN = "access_token"
+    const val REFRESH_TOKEN = "refresh_token"
+    const val TOOLTIP_VISIBLE = "tooltip_visible"
+}
