bafbfb5c40e09cfbfce88b42dcd40b6b5cebe869
YHKOO95 <taleskoo@gmail.com>
2024-01-02 00:33:12 +0900

    [SDR-102] Flipper 도입

--- core-api/src/main/java/com/ddd/sikdorok/core_api/NetworkModule.kt
+++ core-api/src/main/java/com/ddd/sikdorok/core_api/NetworkModule.kt
@@ -1,5 +1,7 @@
 package com.ddd.sikdorok.core_api
 
+import android.content.Context
+import com.ddd.sikdorok.core_api.annotation.FlipperInterceptor
 import com.ddd.sikdorok.core_api.annotation.HeaderInterceptor
 import com.ddd.sikdorok.core_api.annotation.LoggingInterceptor
 import com.ddd.sikdorok.core_api.annotation.NoAuthOkHttpClient
@@ -7,9 +9,13 @@ import com.ddd.sikdorok.core_api.annotation.NoAuthRetrofit
 import com.ddd.sikdorok.core_api.annotation.NormalOkHttpClient
 import com.ddd.sikdorok.core_api.annotation.NormalRetrofit
 import com.ddd.sikdorok.core_api.annotation.RefreshInterceptor
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.plugins.network.FlipperOkhttpInterceptor
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.Authenticator
 import okhttp3.Interceptor
@@ -48,15 +54,20 @@ internal object NetworkModule {
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
+
+    // OKHttpClients
     @Provides
     @Singleton
     @NormalOkHttpClient
     fun providesNormalOkHttpClient(
+        @ApplicationContext context: Context,
         @LoggingInterceptor interceptor: Interceptor,
         @HeaderInterceptor accessTokenInterceptor: Interceptor,
+        @FlipperInterceptor flipperInterceptor: Interceptor,
         @RefreshInterceptor refreshTokenInterceptor: Authenticator
     ) = OkHttpClient.Builder()
         .addInterceptor(interceptor)
+        .addNetworkInterceptor(flipperInterceptor)
         .addInterceptor(accessTokenInterceptor)
         .authenticator(refreshTokenInterceptor)
         .build()
@@ -65,15 +76,24 @@ internal object NetworkModule {
     @Singleton
     @NoAuthOkHttpClient
     fun providesNoAuthOkHttpClient(
-        @LoggingInterceptor interceptor: Interceptor,
+        @ApplicationContext context: Context,
+        @FlipperInterceptor flipperInterceptor: Interceptor,
+        @LoggingInterceptor interceptor: Interceptor
     ) = OkHttpClient.Builder()
+        .addNetworkInterceptor(
+            FlipperOkhttpInterceptor(
+                AndroidFlipperClient.getInstance(context).getPlugin(NetworkFlipperPlugin.ID)
+            )
+        )
         .addInterceptor(interceptor)
         .build()
 
     @Provides
     @Singleton
     @LoggingInterceptor
-    fun providesHttpLoggingInterceptor(): Interceptor {
+    fun providesHttpLoggingInterceptor(
+        @ApplicationContext context: Context
+    ): Interceptor {
         val interceptor = HttpLoggingInterceptor {
             Timber.i(it)
         }
